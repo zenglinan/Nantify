b9565b30046b36abfd757a557138edf85ac0fada
datou <1392767180@qq.com>
2019-07-09 21:02:26 +0800

    complete merge

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12507,7 +12507,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54451" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55125" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
