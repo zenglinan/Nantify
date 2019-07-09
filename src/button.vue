847506cdca7f4ca9e30f0a809fe6dceb21368376
datou <1392767180@qq.com>
2019-07-09 21:00:12 +0800

    add parcel finish

---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="coco c-button">1</button>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style lang="scss">
+.c-button {
+  padding: var(--button-padding);
+  font-size: var(--font-size);
+  border-radius: var(--button-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  color: var(--color);
+  outline: none;
+  cursor: pointer;
+  min-width: 60px;
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+}
+</style>
