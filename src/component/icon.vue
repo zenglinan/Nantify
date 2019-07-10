cc5d3eda340f5ec256d1465d44bfeaa00c9fd367
datou <1392767180@qq.com>
2019-07-10 13:54:59 +0800

    添加button的loading动画可选项

---
+++ src/component/icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="`#${icon}`" />
+  </svg>
+</template>
+
+<script>
+  export default {
+    props: {
+      icon: {
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
