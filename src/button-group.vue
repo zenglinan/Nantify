e845df774a3f2732d129edb4f1a01f0f5ab37ed6
datou <1392767180@qq.com>
2019-07-11 14:56:09 +0800

    接入travis 进行持续集成

---
+++ src/button-group.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="c-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import Button from './button'
+  export default {
+    name: "button-group",
+    components: {
+      'c-button': Button
+    }
+  }
+</script>
+
+<style lang="scss">
+.c-button-group {
+  display: inline-flex;
+  .c-button {
+    border-radius: 0;
+    &:not(:first-child) {  // 消除相邻border的方法
+      margin-left: -1px;
+    }
+    &:hover {
+      position: relative;
+      z-index: 2;
+    }
+    &:first-child {
+      border-top-left-radius: var(--button-radius);
+      border-bottom-left-radius: var(--button-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--button-radius);
+      border-bottom-right-radius: var(--button-radius);
+    }
+  }
+}
+</style>
