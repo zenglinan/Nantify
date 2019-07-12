78b0d78b0adaabeabaec327ce5a7d54155c68970
datou <1392767180@qq.com>
2019-07-12 22:28:12 +0800

    优化 Input 的props的单元测试代码

--- test/input.test.js
+++ test/input.test.js
@@ -10,53 +10,49 @@ describe('Input', () => {
     expect(Input).to.be.ok
   })
   describe('props', () => {
+    const Constructor = Vue.extend(Input)
+    let vm
+    afterEach(() => {
+      vm.$destroy()
+    })
     it('可以接收value', () => {
-      const Constructor = Vue.extend(Input)
-      const vm = new Constructor({
+      vm = new Constructor({
         propsData: {
           value: 'value'
         }
       }).$mount()
       const useElement = vm.$el.querySelector('input')
       expect(useElement.value).to.equal('value')
-      vm.$destroy()
     })
     it('可以接收disabled', () => {
-      const Constructor = Vue.extend(Input)
-      const vm = new Constructor({
+      vm = new Constructor({
         propsData: {
           disabled: true
         }
       }).$mount()
       const useElement = vm.$el.querySelector('input')
       expect(useElement.disabled).to.equal(true)
-      vm.$destroy()
     })
     it('可以接收placeholder', () => {
-      const Constructor = Vue.extend(Input)
-      const vm = new Constructor({
+      vm = new Constructor({
         propsData: {
           placeholder: '代替文字'
         }
       }).$mount()
       const useElement = vm.$el.querySelector('input')
       expect(useElement.placeholder).to.equal('代替文字')
-      vm.$destroy()
     })
     it('可以接收errorPrompt', () => {
-      const Constructor = Vue.extend(Input)
-      const vm = new Constructor({
+      vm = new Constructor({
         propsData: {
           errorPrompt: true
         }
       }).$mount()
       const useElement = vm.$el
       expect(Array.from(useElement.classList)).to.include('error')
-      vm.$destroy()
     })
     it('可以接收errorMessage', () => {
-      const Constructor = Vue.extend(Input)
-      const vm = new Constructor({
+      vm = new Constructor({
         propsData: {
           errorMessage: '错误信息',
           errorPrompt: true
@@ -65,7 +61,6 @@ describe('Input', () => {
       const useElement = vm.$el.querySelector('span')
       console.log(vm.$el);
       expect(useElement.innerHTML).to.equal('错误信息')
-      vm.$destroy()
     })
   })
 
