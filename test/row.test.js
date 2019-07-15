5dba52117a19756ba69e40cd110c84ba51533d33
datou <1392767180@qq.com>
2019-07-15 14:30:01 +0800

    将测试断言通过setTimeOut放在异步队列里面，但测试用例仍不通过

---
+++ test/row.test.js
@@ -0,0 +1,38 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Row from '../src/row'
+import Col from '../src/col'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Row', () => {
+
+  it('存在.', () => {
+    expect(Row).to.exist
+  })
+  it('可以接收gutter 并设置正确的margin padding', () => {
+    Vue.component('c-row', Row)
+    Vue.component('c-col', Col)
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <c-row gutter="10">
+        <c-col></c-col>
+        <c-col></c-col>
+      <c-row>
+    `
+    const vm = new Vue({
+      el: "div"
+    })
+    setTimeout(() => {
+      const row = vm.$el.querySelector('.c-row')
+      const col = vm.$el.querySelector('.c-col')
+      expect(getComputedStyle(row).marginLeft).to.eq('-5px')
+      expect(getComputedStyle(row).marginRight).to.eq('-5px')
+      expect(getComputedStyle(col).paddingLeft).to.eq('5px')
+      expect(getComputedStyle(row).paddingLeft).to.eq('5px')
+    },0)
+  })
+
+})
