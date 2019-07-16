e5f2c3b876d4440668b5229b50bd7f5d4d98c913
datou <1392767180@qq.com>
2019-07-16 23:34:47 +0800

    添加Toast动画 淡入、上滑、下滑，消失动画还未做

--- src/Toast.vue
+++ src/Toast.vue
@@ -46,21 +46,51 @@
 </script>
 
 <style scoped lang="scss">
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 100%;
+    }
+  }
+
+  ;
+  @keyframes slideUp {
+    0% {
+      transform: translate(-50%, 100%);
+    }
+    100% {
+      transform: translateX(-50%);
+    }
+  }
+
+  @keyframes slideDown {
+    0% {
+      transform: translate(-50%, -100%);
+    }
+    100% {
+      transform: translateX(-50%);
+    }
+  }
+
   $font-size: 14px;
   $toast-min-height: 42px;
   $toast-bg: rgba(0, 0, 0, 0.65);
   $border-radius: 4px;
+  $animation-time: .3s;
   .c-toast {
+    animation: fadeIn $animation-time;
     font-size: $font-size; height: $toast-min-height; line-height: 1.8;
     position: fixed; left: 50%; transform: translateX(-50%); display: flex;
     color: white;align-items: center;background: $toast-bg;border-radius: $border-radius;
     box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.3);padding: 0 60px;
 
-    &.toast-top {top: 10px;}
+    &.toast-top {top: 10px; animation: slideDown $animation-time;}
 
     &.toast-middle {top: 50%; transform: translate(-50%, -50%)}
 
-    &.toast-bottom {bottom: 10px;}
+    &.toast-bottom {bottom: 10px; animation: slideUp $animation-time;}
 
     & .c-close {cursor: pointer;background-position: center;background-image: url("../asset/close.png");
       background-size: cover;content: '';height: 10px;display: block;position: absolute;
