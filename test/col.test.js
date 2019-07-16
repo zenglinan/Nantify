c3b87f91a7c68ccc51c4569f46208fe695ceccfb
datou <1392767180@qq.com>
2019-07-17 00:51:38 +0800

    上传一些webstorm漏传的文件

---
+++ test/col.test.js
@@ -0,0 +1,36 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Row from '../src/Row'
+import Col from '../src/Col'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Col', () => {
+  const Constructor = Vue.extend(Col)
+  let vm
+  const div = document.createElement('div')
+  it('存在.', () => {
+    expect(Col).to.exist
+  })
+  it('可以接收span 并设置对应的类名', () => {
+    vm = new Constructor({
+      propsData: {
+        span: 12
+      }
+    }).$mount(div)
+    let col = vm.$el
+    expect(col.classList.contains('col-12')).to.eq(true)
+  })
+  it('可以接收offset 并设置对应的类名', () => {
+    vm = new Constructor({
+      propsData: {
+        span: 12,
+        offset: 6
+      }
+    }).$mount(div)
+    let col = vm.$el
+    expect(col.classList.contains('offset-6')).to.eq(true)
+  })
+
+})
