3c9fd4f8b06819457f225a5efcc5fb52dbaa40ba
datou <1392767180@qq.com>
2019-07-17 11:53:11 +0800

    完成Toast组件的测试用例

--- test/toast.test.js
+++ test/toast.test.js
@@ -7,15 +7,15 @@ Vue.config.devtools = false
 
 describe('Toast', () => {
   const Constructor = Vue.extend(Toast)
+  let vm
   it('存在.', () => {
     expect(Toast).to.exist
   })
   describe('props', () => {
-    it('接受 autoClose', (done) => {
+    it('接受 autoClose closeDelay', (done) => {
       let div = document.createElement('div')
       document.body.appendChild(div)
-      const Constructor = Vue.extend(Toast)
-      const vm = new Constructor({
+      vm = new Constructor({
         propsData: {
           autoClose: true,
           closeDelay: 1
@@ -24,7 +24,46 @@ describe('Toast', () => {
       setTimeout(() => {
         expect(document.body.contains(vm.$el)).to.eq(false)
         done()
-      },1000)
+      }, 1000)
+    })
+    it('可以接收callback', (done) => {
+      const callback = sinon.fake()
+      vm = new Constructor({
+        propsData: {
+          autoClose: true,
+          closeDelay: 1,
+          callback
+        }
+      }).$mount()
+      setTimeout(() => {
+        expect(callback).to.have.been.called
+        done()
+      }, 1000)
+
+    })
+    it('可以接收position', () => {
+      vm = new Constructor({
+        propsData: {
+          autoClose: false,
+          position: 'top'
+        }
+      }).$mount()
+      expect(vm.$el.classList.contains('toast-top')).to.eq(true)
+    })
+  })
+
+  describe('事件', () => {
+    it('可以触发close事件', () => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      vm = new Constructor({
+        propsData:{
+          autoClose: false
+        }
+      }).$mount(div)
+      let closeButton = vm.$el.querySelector('.c-close')
+      closeButton.click()
+      expect(document.body.contains(vm.$el)).to.eq(false)
     })
   })
 
